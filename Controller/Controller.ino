b72c8c6e30de5579a96a80a9c22d84ced98a2586
Sunshine <elijahmiller@hotmail.com>
2017-09-23 13:57:46 -0400

    Refactored with generateOpCode function

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -7,7 +7,6 @@ String str1;
 String str2;
 String str3;
 String str4;
-String str5;
 String str6;
 
 void setup() 
@@ -23,26 +22,8 @@ int ST1 = analogRead(A1); //Tilting Servo
 int FB1 = analogRead(A2); //Forward/Backward
 int LR1 = analogRead(A3); //Left/Right
 
-str5 = "N";
+String str5 = generateOpCode(FB1, LR1);
 
-if (FB1 > 768)
-{
-   str5 = "F";
-}
-else if (FB1 < 256)
-{
-  str5 = "B";
-}
-
-if (LR1 > 768)
-{
-  str5 = "R";
-  }
-
-else if (LR1 < 256)
-{
-  str5 = "L";
-  }
 
 str3 = map(SP1, 0, 1024, 0, 9);
 str4 = map(ST1, 0, 1024, 0, 9);
@@ -56,4 +37,32 @@ DATA = str1 + str3 + str2 + str4 + str2 + str5 + str11;
 Serial.println(DATA);
 
 delay(1000);
+}
+
+
+String generateOpCode (int FB1, int LR1)
+{
+  
+  String str5 = "N";
+
+  if (FB1 > 768)
+  {
+     str5 = "F";
+  }
+  else if (FB1 < 256)
+  {
+    str5 = "B";
+  }
+  
+  if (LR1 > 768)
+  {
+    str5 = "R";
+    }
+  
+  else if (LR1 < 256)
+  {
+    str5 = "L";
+    }
+
+  return str5;
 }
