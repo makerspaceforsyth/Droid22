d42efb52fab1efa41d9e906984d05d3d70fa5e0f
Sunshine <elijahmiller@hotmail.com>
2017-09-23 14:50:24 -0400

    Finished variable refactoring

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,4 +1,4 @@
-//Version 1.1
+//Version 1.2
 //0, 1024, 512, 768, 256;
 //Control scketch for Maskerspace Mascot
 
@@ -9,53 +9,52 @@ String opCode;
 
 void setup() 
 {
-Serial.begin(9600); 
+  Serial.begin(9600); 
 }
 
 void loop() 
 {
 
-int SP1 = analogRead(A0); //Panning Servo
-int ST1 = analogRead(A1); //Tilting Servo
-int FB1 = analogRead(A2); //Forward/Backward
-int LR1 = analogRead(A3); //Left/Right
-
-opCode = generateOpCode(FB1, LR1);
-
-servoHead = map(SP1, 0, 1024, 0, 9);
-servoTilt = map(ST1, 0, 1024, 0, 9);
- 
-DATA = "<" + servoHead + "," + servoTilt + "," + opCode + ">";
-
-Serial.println(DATA);
-
-delay(1000);
+  int SP1 = analogRead(A0); //Panning Servo
+  int ST1 = analogRead(A1); //Tilting Servo
+  int FB1 = analogRead(A2); //Forward/Backward
+  int LR1 = analogRead(A3); //Left/Right
+  
+  opCode = generateOpCode(FB1, LR1);
+  
+  servoHead = map(SP1, 0, 1024, 0, 9);
+  servoTilt = map(ST1, 0, 1024, 0, 9);
+   
+  DATA = "<" + servoHead + "," + servoTilt + "," + opCode + ">";
+  
+  Serial.println(DATA);
+  
+  delay(1000);
 }
 
 
 String generateOpCode (int FB1, int LR1)
-{
-  
-  String str5 = "N";
+{  
+  String op = "N";
 
   if (FB1 > 768)
   {
-     str5 = "F";
+     op = "F";
   }
   else if (FB1 < 256)
   {
-    str5 = "B";
+    op = "B";
   }
   
   if (LR1 > 768)
   {
-    str5 = "R";
+    op = "R";
     }
   
   else if (LR1 < 256)
   {
-    str5 = "L";
+    op = "L";
     }
 
-  return str5;
+  return op;
 }
