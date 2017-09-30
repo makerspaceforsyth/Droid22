fd23a675af3d3c7f49457f6e568e30e3b7d13974
Alexander The Great <kmillio1@my.westga.edu>
2017-09-29 18:51:49 -0800

    Fixed Pin Assignment of wheels

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -10,6 +10,10 @@ String opCode;
 int SP1;
 int ST1;
 
+const int LeftWheelPowerControl = 42;
+const int LeftWheelSpeedControl = 43;
+const int RightWheelPowerControl = 44;
+const int RightWheelSpeedControl = 45;
 
 #include <Servo.h>;
 
@@ -61,45 +65,45 @@ void Movement ()
 {
   if (opCode.substring(0) == "F")
   {
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(8, HIGH);
+    digitalWrite(LeftWheelPowerControl, HIGH);
+    digitalWrite(LeftWheelSpeedControl, HIGH);
+    digitalWrite(RightWheelPowerControl, HIGH);
+    digitalWrite(RightWheelSpeedControl, HIGH);
     Serial.println("Robot is Moving Forward.");
     Serial.println("");
   }
   else if (opCode.substring(0) == "B")
   {
-    digitalWrite(2,HIGH);
-    digitalWrite(4,LOW);
-    digitalWrite(7,HIGH);
-    digitalWrite(8,LOW);
+    digitalWrite(LeftWheelPowerControl, HIGH);
+    digitalWrite(LeftWheelSpeedControl, LOW);
+    digitalWrite(RightWheelPowerControl, HIGH);
+    digitalWrite(RightWheelSpeedControl, LOW);
     Serial.println("Robot is Moving Backward.");
     Serial.println("");
   }
   else if (opCode == "L")
   {
-    digitalWrite(2,LOW);
-    digitalWrite(4,LOW);
-    digitalWrite(7,HIGH);
-    digitalWrite(8,HIGH);
+    digitalWrite(LeftWheelPowerControl, LOW);
+    digitalWrite(LeftWheelSpeedControl, LOW);
+    digitalWrite(RightWheelPowerControl, HIGH);
+    digitalWrite(RightWheelSpeedControl, HIGH);
     Serial.println("Robot is Turning Left.");
     Serial.println("");
   }
   else if (opCode == "R")
   {
-    digitalWrite(2,HIGH);
-    digitalWrite(4,HIGH);
-    digitalWrite(7,LOW);
-    digitalWrite(8,LOW);
+    digitalWrite(LeftWheelPowerControl, HIGH);
+    digitalWrite(LeftWheelSpeedControl, HIGH);
+    digitalWrite(RightWheelPowerControl, LOW);
+    digitalWrite(RightWheelSpeedControl, LOW);
     Serial.println("Robot is Turning Right.");
     Serial.println("");
   }
   else {
-    digitalWrite(2,LOW);
-    digitalWrite(4,LOW);
-    digitalWrite(7,LOW);
-    digitalWrite(8,LOW);
+    digitalWrite(LeftWheelPowerControl, LOW);
+    digitalWrite(LeftWheelSpeedControl, LOW);
+    digitalWrite(RightWheelPowerControl, LOW);
+    digitalWrite(RightWheelSpeedControl, LOW);
     Serial.println("Robot is Still.");
     Serial.println("");
   }
