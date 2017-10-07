c50736e20fa4e7ae98792bbb66e979f0be2b49cb
Alexander The Great <kmillio1@my.westga.edu>
2017-10-07 13:23:39 -0800

    Working Speed Control

---
+++ Speed_Control_For_Mascot/Speed_Control_For_Mascot.ino
@@ -0,0 +1,22 @@
+void setup() 
+{
+pinMode(51, OUTPUT);
+digitalWrite(51, HIGH);
+pinMode(42, OUTPUT);
+pinMode(43, OUTPUT);
+pinMode(44, OUTPUT);
+pinMode(45, OUTPUT);
+digitalWrite(42, LOW);
+digitalWrite(43, LOW);
+}
+
+void loop() 
+{
+analogWrite(44, 10);
+analogWrite(45, 50);
+digitalWrite(51, LOW);
+delay(50);
+digitalWrite(42, HIGH);
+digitalWrite(43, HIGH);
+delay(1000);
+}
